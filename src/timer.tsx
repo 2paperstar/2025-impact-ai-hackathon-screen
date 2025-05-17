e2a71f5dd2c2d1af01438e79b5e4f584f3cdd4ab
2paperstar <lee@paperst.ar>
2025-05-18 02:43:42 +0900

    feat: format with binary number

--- src/timer.tsx
+++ src/timer.tsx
@@ -2,16 +2,16 @@ import { useEffect, useState } from "react";
 
 const targetDate = new Date("2025-05-18T13:00:00");
 
-const formatTime = (time: number) => {
+const formatTime = (time: number, base: number = 10, length: number = 2) => {
   const hours = Math.floor(time / 3600000)
-    .toString()
-    .padStart(2, "0");
+    .toString(base)
+    .padStart(length, "0");
   const minutes = Math.floor((time % 3600000) / 60000)
-    .toString()
-    .padStart(2, "0");
+    .toString(base)
+    .padStart(length, "0");
   const seconds = Math.floor((time % 60000) / 1000)
-    .toString()
-    .padStart(2, "0");
+    .toString(base)
+    .padStart(length, "0");
   return `${hours}:${minutes}:${seconds}`;
 };
 
@@ -26,5 +26,10 @@ export const Timer = () => {
     return () => clearInterval(interval);
   }, []);
 
-  return formatTime(time);
+  return (
+    <div className="flex flex-col items-center ">
+      <div>{formatTime(time, 2, 8)}</div>
+      <div className="text-[60px] opacity-40">{formatTime(time)}</div>
+    </div>
+  );
 };
