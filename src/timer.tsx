65a161d2955390d3a89cdce1c35a346eccf2dd71
2paperstar <lee@paperst.ar>
2025-05-17 15:54:27 +0900

    feat: timer padding zero

--- src/timer.tsx
+++ src/timer.tsx
@@ -3,9 +3,15 @@ import { useEffect, useState } from "react";
 const targetDate = new Date("2025-05-18T13:00:00");
 
 const formatTime = (time: number) => {
-  const hours = Math.floor(time / 3600000);
-  const minutes = Math.floor((time % 3600000) / 60000);
-  const seconds = Math.floor((time % 60000) / 1000);
+  const hours = Math.floor(time / 3600000)
+    .toString()
+    .padStart(2, "0");
+  const minutes = Math.floor((time % 3600000) / 60000)
+    .toString()
+    .padStart(2, "0");
+  const seconds = Math.floor((time % 60000) / 1000)
+    .toString()
+    .padStart(2, "0");
   return `${hours}:${minutes}:${seconds}`;
 };
 
