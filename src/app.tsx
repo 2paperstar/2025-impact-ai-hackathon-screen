76fd6058aec2cfbde70f29bd1b11ca60ef33ece2
2paperstar <lee@paperst.ar>
2025-05-17 16:09:12 +0900

    feat: add gradient animation

--- src/app.tsx
+++ src/app.tsx
@@ -2,6 +2,18 @@ import { Background } from "./background";
 import { useAuthRefresh } from "./hooks/use-auto-refresh";
 import { Timer } from "./timer";
 
+const Title = () => {
+  return (
+    <div className="font-[Kdam_Thmor_Pro] text-[100px] flex gap-4">
+      🔥{" "}
+      <div className="bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 bg-clip-text text-transparent animate-gradient-flow">
+        Solve with Code
+      </div>{" "}
+      🔥
+    </div>
+  );
+};
+
 export const App = () => {
   useAuthRefresh();
 
@@ -11,9 +23,7 @@ export const App = () => {
       <div className="fixed inset-0 flex flex-col items-center justify-center gap-10">
         <div className="flex flex-col items-center">
           <div className="text-[240px] -my-12">⌛</div>
-          <div className="font-[Kdam_Thmor_Pro] text-[100px]">
-            🔥 Solve with Code 🔥
-          </div>
+          <Title />
         </div>
         <div className="font-[Kdam_Thmor_Pro] text-[100px]">
           <Timer />
