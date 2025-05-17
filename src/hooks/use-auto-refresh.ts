1131cca60895c60ae82836fc1cfac4f16c37758f
2paperstar <lee@paperst.ar>
2025-05-17 16:18:06 +0900

    fix: change auto refresh code

--- src/hooks/use-auto-refresh.ts
+++ src/hooks/use-auto-refresh.ts
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 
-export const useAuthRefresh = (duration: number = 1000 * 60) => {
+export const useAuthRefresh = (duration: number = 1000 * 10) => {
   useEffect(() => {
     const interval = setInterval(() => {
       window.location.reload();
