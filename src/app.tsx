a85f2dc2e6a4aeb7548bee6539fe917bc7712f3f
2paperstar <lee@paperst.ar>
2025-05-17 17:31:10 +0900

    fix: change font size

--- src/app.tsx
+++ src/app.tsx
@@ -27,7 +27,7 @@ export const App = () => {
         <div className="font-[Kdam_Thmor_Pro] text-[80px]">
           <Timer />
         </div>
-        <div className="text-center text-4xl flex flex-col gap-2">
+        <div className="text-center text-[40px] flex flex-col gap-2">
           <div className="flex flex-col items-center">
             <span className="bg-white/60 px-2 py-1">
               저녁이 준비되었습니다! 운영진 안내에 따라 식사를 수령해 주세요
@@ -47,19 +47,19 @@ export const App = () => {
             </span>
           </div>
         </div>
-        <div className="flex items-center gap-4 text-3xl">
+        <div className="flex items-center gap-4 text-[32px]">
           Menus:
           <div className="flex flex-col items-center bg-red-100 p-2">
             <div>오징어 두루치기</div>
-            <div className="text-xl">Squid Stir-Fry</div>
+            <div className="text-[24px]">Squid Stir-Fry</div>
           </div>
           <div className="flex flex-col items-center bg-yellow-100 p-2">
             <div>버섯소불고기</div>
-            <div className="text-xl">Mushroom Bulgogi</div>
+            <div className="text-[24px]">Mushroom Bulgogi</div>
           </div>
           <div className="flex flex-col items-center bg-blue-100 p-2">
             <div>숯불닭구이</div>
-            <div className="text-xl">Grilled Chicken</div>
+            <div className="text-[24px]">Grilled Chicken</div>
           </div>
         </div>
       </div>
