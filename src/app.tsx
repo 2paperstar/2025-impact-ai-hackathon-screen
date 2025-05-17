84170239102b79a41a556350179aa19144b36428
2paperstar <lee@paperst.ar>
2025-05-17 15:45:09 +0900

    feat: UI

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,18 @@
+import { Background } from "./background";
+
 export const App = () => {
-  return <>test</>;
+  return (
+    <>
+      <Background />
+      <div className="fixed inset-0 flex flex-col items-center justify-center gap-10">
+        <div className="flex flex-col items-center">
+          <div className="text-[240px] -my-12">⌛</div>
+          <div className="font-[Kdam_Thmor_Pro] text-[100px]">
+            🔥 Solve with Code 🔥
+          </div>
+        </div>
+        <div className="font-[Kdam_Thmor_Pro] text-[100px]">10:43:24</div>
+      </div>
+    </>
+  );
 };
---
+++ src/background.tsx
@@ -0,0 +1,17 @@
+import ArrowIcon from "./assets/arrow.svg?react";
+import ColonIcon from "./assets/colon.svg?react";
+import DotsIcon from "./assets/dots.svg?react";
+import StarIcon from "./assets/star.svg?react";
+
+export const Background = () => {
+  return (
+    <div className="fixed inset-0 bg-[#EEEEEE]">
+      <div className=" opacity-40">
+        <StarIcon className="absolute top-1/4 left-1/12 size-[300px]" />
+        <ArrowIcon className="absolute top-1/2 left-1/3 size-[300px]" />
+        <DotsIcon className="absolute top-1/4 left-1/2 size-[300px]" />
+        <ColonIcon className="absolute top-1/2 right-1/12 size-[300px]" />
+      </div>
+    </div>
+  );
+};
