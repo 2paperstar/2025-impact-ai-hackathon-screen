6b20e2230572139c3766c44c67c45e1e3806acbf
2paperstar <lee@paperst.ar>
2025-05-17 18:41:28 +0900

    feat: revert to main screen

--- src/app.tsx
+++ src/app.tsx
@@ -5,11 +5,11 @@ import { Timer } from "./timer";
 const Title = () => {
   return (
     <div className="font-[Kdam_Thmor_Pro] text-[100px] flex gap-4">
-      🍱{" "}
-      <div className="bg-gradient-to-r from-blue-400 via-green-500 to-yellow-500 bg-clip-text text-transparent animate-gradient-flow">
-        Dinner Time
-      </div>
-      🍱
+      🔥{" "}
+      <div className="bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 bg-clip-text text-transparent animate-gradient-flow">
+        Solve with Code!!
+      </div>{" "}
+      🔥
     </div>
   );
 };
@@ -20,52 +20,15 @@ export const App = () => {
   return (
     <>
       <Background />
-      <div className="fixed inset-0 flex flex-col items-center justify-center gap-12">
-        <div className="flex flex-col gap-4 items-center">
-          <div className="flex flex-col items-center">
-            <Title />
+      <div className="fixed inset-0 flex flex-col items-center justify-center gap-10">
+        <div className="flex flex-col items-center">
+          <div className="text-[240px] -my-12 animate-rotate-with-delay">
+            ⌛
           </div>
-          <div className="font-[Kdam_Thmor_Pro] text-[80px]">
-            <Timer />
-          </div>
+          <Title />
         </div>
-        <div className="flex flex-col gap-4 items-center">
-          <div className="text-center text-[40px] font-bold flex flex-col gap-2">
-            <div className="flex flex-col items-center">
-              <span className="bg-white/60 px-2 py-1">
-                저녁이 준비되었습니다! 운영진 안내에 따라 식사를 수령해 주세요
-              </span>
-              <span className="bg-white/60 px-2 py-1">
-                다 드신 후에는 문 밖에 있는 분리수거함 라벨에 맞게{" "}
-                <span className="text-blue-400">분리수거</span> 해주세요
-              </span>
-            </div>
-            <div className="flex flex-col items-center">
-              <span className="bg-white/60 px-2 py-1">
-                Dinner is available outside! Follow the staff instructions to
-                collect your meal.
-              </span>
-              <span className="bg-white/60 px-2 py-1">
-                Dispose of your food according to the labels on the{" "}
-                <span className="text-blue-400">recycling bin</span>
-              </span>
-            </div>
-          </div>
-          <div className="flex items-center gap-4 text-[32px] font-semibold">
-            Menus:
-            <div className="flex flex-col items-center bg-red-100 p-2">
-              <div>오징어 두루치기</div>
-              <div className="text-[24px]">Squid Stir-Fry</div>
-            </div>
-            <div className="flex flex-col items-center bg-yellow-100 p-2">
-              <div>버섯소불고기</div>
-              <div className="text-[24px]">Mushroom Bulgogi</div>
-            </div>
-            <div className="flex flex-col items-center bg-blue-100 p-2">
-              <div>숯불닭구이</div>
-              <div className="text-[24px]">Grilled Chicken</div>
-            </div>
-          </div>
+        <div className="font-[Kdam_Thmor_Pro] text-[100px]">
+          <Timer />
         </div>
       </div>
     </>
