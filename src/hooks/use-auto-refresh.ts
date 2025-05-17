1480b230ac1154ab9379a779d1393ffc819f34b6
2paperstar <lee@paperst.ar>
2025-05-18 07:06:02 +0900

    fix: auto refresh with compare index.html file content

--- src/hooks/use-auto-refresh.ts
+++ src/hooks/use-auto-refresh.ts
@@ -1,9 +1,26 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 
 export const useAutoRefresh = (duration: number = 1000 * 60) => {
+  const originalContent = useRef<string>();
+
+  useEffect(() => {
+    fetch("/2025-impact-ai-hackathon-screen/").then((res) =>
+      res.text().then((text) => {
+        originalContent.current = text.trim();
+      })
+    );
+  }, []);
+
   useEffect(() => {
     const interval = setInterval(() => {
-      window.location.reload();
+      if (!originalContent.current) return;
+      fetch("/2025-impact-ai-hackathon-screen/").then((res) =>
+        res.text().then((text) => {
+          if (originalContent.current !== text.trim()) {
+            window.location.reload();
+          }
+        })
+      );
     }, duration);
     return () => clearInterval(interval);
   }, [duration]);
