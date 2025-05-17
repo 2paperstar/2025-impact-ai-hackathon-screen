d802514b483034a2106b06d267d330794bb1460a
2paperstar <lee@paperst.ar>
2025-05-17 16:02:23 +0900

    feat: change auto refresh default value

--- src/hooks/use-auto-refresh.ts
+++ src/hooks/use-auto-refresh.ts
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 
-export const useAuthRefresh = (duration: number = 1000 * 60 * 5) => {
+export const useAuthRefresh = (duration: number = 1000 * 60) => {
   useEffect(() => {
     const interval = setInterval(() => {
       window.location.reload();
